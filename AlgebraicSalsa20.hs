37264e8788f13d92d2ba73645a8dbfb7b29249e1
Ken T Takusagawa <kenta@mit.edu>
2015-04-17 01:32:02 -0400

    delete unused code

--- AlgebraicSalsa20.hs
+++ AlgebraicSalsa20.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 -- enough rope to evaluate Salsa20 algebraically
-module AlgebraicSalsa20 where {
+module Main(main) where {
 import Data.Bits(Bits(..));
-import Data.List(sortBy,transpose);
-import Data.Ord(comparing);
+import Data.List(transpose);
 import Salsa20;
 data Algebraic a = Atom a | Xor (Algebraic a) (Algebraic a) | Rotate (Algebraic a) Int | Add (Algebraic a) (Algebraic a) deriving (Show,Eq);
 
@@ -40,40 +39,16 @@ Rotate x i -> Rotate (simplify v x) i;
 Add x y -> Add (simplify v x) (simplify v y);
 };
 
-size :: Algebraic a -> Integer;
-size (Atom _) = 1;
-size (Xor x y) = 1 + size x + size y;
-size (Add x y) = 1 + size x + size y;
-size (Rotate x _) = 1 + size x;
-
 simplify_in_order :: [Assign] -> [Assign];
 simplify_in_order [] = [];
 simplify_in_order (f@(Assign v1 e1):rest) = f:(simplify_in_order
 $ map (\(Assign v e) -> Assign v $ simplify ((v1,Updated), e1) e) rest);
-
-map_fst :: (a -> b) -> [(a,c)] -> [(b,c)];
-map_fst f l = do {
-x <- l;
-return (f $ fst x, snd x);
-};
-
-do_by_size :: forall a b sortable . (Ord sortable) => ([a] -> [b]) -> (a -> sortable) -> [a] -> [b];
-do_by_size f getsize l = let {
- sz_and :: [(a, Integer)];
- sz_and = sortBy (comparing $ getsize . fst) $ zip l $ enumFrom 0;
-} in map snd $ sortBy (comparing fst) $ zip (map snd sz_and) $ f $ map fst sz_and;
-
-simplify_by_size :: [Assign] -> [Assign];
-simplify_by_size = do_by_size simplify_in_order $ size . (\(Assign _ e) -> e);
 
 algebraic_show :: (Algebraic (Vnum,a)) -> String;
 algebraic_show (Atom ((Vnum i),_)) = "x[" ++ show i ++ "]";
 algebraic_show (Add x y) = "(" ++ algebraic_show x ++ " + " ++ algebraic_show y ++ ")";
 algebraic_show (Xor x y) = algebraic_show x ++ " ^ " ++ algebraic_show y;
 algebraic_show (Rotate x k) = "(" ++ algebraic_show x ++ " <<< " ++ show k ++ ")";
-
-test_shift_columns :: [[Algebraic Integer]];
-test_shift_columns = shift_columns $ transpose $ to_matrix 4 $ map Atom [0..15];
 
 run_algebraic :: [Vnum] -> [Assign];
 run_algebraic order = zipWith Assign order (column $ do { n <- order; return $ Atom (n,Original)});
@@ -97,4 +72,6 @@ newtype Vnum = Vnum Integer deriving (Show,Eq);
 
 data Assign = Assign Vnum Expr;
 
+main :: IO();
+main = mapM_ putStrLn c_code_out;
 }
