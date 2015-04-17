15d0d6130ef629773a6939246b0a50a7c7989dd8
Ken T Takusagawa <kenta@mit.edu>
2015-04-17 01:26:54 -0400

    Polish

--- AlgebraicSalsa20.hs
+++ AlgebraicSalsa20.hs
@@ -46,9 +46,10 @@ size (Xor x y) = 1 + size x + size y;
 size (Add x y) = 1 + size x + size y;
 size (Rotate x _) = 1 + size x;
 
-simplify_in_order :: [(Algebraic (Integer,Bool),Integer)] -> [(Algebraic (Integer,Bool),Integer)];
+simplify_in_order :: [Assign] -> [Assign];
 simplify_in_order [] = [];
-simplify_in_order ((e,n):rest) = (e,n):(simplify_in_order $ map_fst (simplify ((n,True), e)) rest);
+simplify_in_order (f@(Assign v1 e1):rest) = f:(simplify_in_order
+$ map (\(Assign v e) -> Assign v $ simplify ((v1,Updated), e1) e) rest);
 
 map_fst :: (a -> b) -> [(a,c)] -> [(b,c)];
 map_fst f l = do {
@@ -60,31 +61,40 @@ do_by_size :: forall a b sortable . (Ord sortable) => ([a] -> [b]) -> (a -> sort
 do_by_size f getsize l = let {
  sz_and :: [(a, Integer)];
  sz_and = sortBy (comparing $ getsize . fst) $ zip l $ enumFrom 0;
-} in map snd $ sortBy (comparing fst) $ zip (map snd sz_and) (f $ map fst sz_and);
+} in map snd $ sortBy (comparing fst) $ zip (map snd sz_and) $ f $ map fst sz_and;
 
-simplify_by_size :: [(Algebraic (Integer,Bool), Integer)] -> [(Algebraic (Integer,Bool),Integer)];
-simplify_by_size = do_by_size simplify_in_order (size . fst);
+simplify_by_size :: [Assign] -> [Assign];
+simplify_by_size = do_by_size simplify_in_order $ size . (\(Assign _ e) -> e);
 
-algebraic_show :: (Algebraic (Integer,a)) -> String;
-algebraic_show (Atom (i,_)) = "x[" ++ show i ++ "]";
+algebraic_show :: (Algebraic (Vnum,a)) -> String;
+algebraic_show (Atom ((Vnum i),_)) = "x[" ++ show i ++ "]";
 algebraic_show (Add x y) = "(" ++ algebraic_show x ++ " + " ++ algebraic_show y ++ ")";
 algebraic_show (Xor x y) = algebraic_show x ++ " ^ " ++ algebraic_show y;
 algebraic_show (Rotate x k) = "(" ++ algebraic_show x ++ " <<< " ++ show k ++ ")";
 
-atest :: [Integer] -> [(Algebraic (Integer,Bool),Integer)];
-atest order = zip (column $ do { n <- order; return $ Atom (n,False)}) order;
--- list_rotate 2 is because the output lags by 2 (arity) from the input.
-
 test_shift_columns :: [[Algebraic Integer]];
 test_shift_columns = shift_columns $ transpose $ to_matrix 4 $ map Atom [0..15];
 
-atest_out :: [Integer] -> [(Algebraic (Integer,Bool),Integer)];
-atest_out = simplify_in_order . (list_rotate $ unArity salsa20_arity) . atest;
+run_algebraic :: [Vnum] -> [Assign];
+run_algebraic order = zipWith Assign order (column $ do { n <- order; return $ Atom (n,Original)});
+-- list_rotate 2 is because the output lags by 2 (arity) from the input.
 
-output_c :: ((Algebraic (Integer, a)), Integer) -> String;
-output_c (e,i) = "x[" ++ show i ++ "] := " ++ algebraic_show e;
+simplify_run :: [Assign] -> [Assign];
+simplify_run = simplify_in_order . (list_rotate $ unArity salsa20_arity);
+
+output_c :: Assign -> String;
+output_c (Assign (Vnum i) e) = "x[" ++ show i ++ "] := " ++ algebraic_show e;
 
 c_code_out :: [String];
-c_code_out = map output_c $ concatMap atest_out $ (shift_columns $ transpose int_matrix) ++ shift_columns int_matrix;
+c_code_out = map output_c $ concatMap (simplify_run . run_algebraic . map Vnum) $ (shift_columns $ transpose int_matrix) ++ shift_columns int_matrix;
+
+-- the original value of a variable, or the updated value.
+-- Fortunately, we do not update a variable more than once.
+data Epoch = Original | Updated deriving (Show, Eq);
+
+type Expr = Algebraic (Vnum, Epoch);
+newtype Vnum = Vnum Integer deriving (Show,Eq);
+
+data Assign = Assign Vnum Expr;
 
 }
