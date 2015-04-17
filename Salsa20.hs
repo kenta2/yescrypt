c4c56578ce587cce803a7a54f8c5ba919b48f0a1
Ken T Takusagawa <kenta@mit.edu>
2015-04-17 02:15:52 -0400

    remove unused code, including transpose_matrix

--- Salsa20.hs
+++ Salsa20.hs
@@ -1,13 +1,10 @@
-{-# LANGUAGE ScopedTypeVariables, LambdaCase, AllowAmbiguousTypes #-}
-module Salsa20 where {
+{-# LANGUAGE ScopedTypeVariables, LambdaCase #-}
+module Salsa20(salsa20_test, int_matrix, shift_columns, salsa20_arity, unArity, list_rotate, column) where {
 import Control.Exception(assert);
 import Data.Typeable(Typeable,cast);
 import Data.Word;
 import Data.Bits(rotate,xor,Bits);
-import Data.Array.ST(STArray);
-import Data.Array.IArray;
 import Data.List;
-
 
 -- import Debug.Trace;
 import Text.Printf;
@@ -19,27 +16,9 @@ type W = Word32;
 round_func :: (Bits a, Num a) => Rotation -> a -> a -> a -> a;
 round_func (Rotation k) a b c = xor c $ rotate (b + a) k;
 
-type Coord = (Integer,Integer);
-
-type Concrete s = STArray s Coord W;
-
 newtype Arity = Arity Integer deriving (Show);
 unArity :: Arity -> Integer;
 unArity (Arity n) = n;
-
-flip_tuple :: (a,b) -> (b,a);
-flip_tuple (x,y) = (y,x);
-
-transpose_array :: forall a i1 i2 e . (Ix i1, Ix i2, IArray a e) => a (i1,i2) e -> a (i2,i1) e;
-transpose_array old = let
-{ bounds_new :: ((i2,i1),(i2,i1))
-; bounds_new = (flip_tuple $ fst $ bounds old, flip_tuple $ snd $ bounds old)
-; f :: (i2, i1) -> e
-; f x = old ! (flip_tuple x); -- ! is already bounds checked
-} in array bounds_new $ zip_map f $ range bounds_new;
-
-zip_map :: (a -> b) -> [a] -> [(a,b)];
-zip_map f l = zip l $ map f l;
 
 list_rotate :: Integer -> [a] -> [a];
 list_rotate n l = if n>=0
@@ -70,19 +49,11 @@ salsa20_arity = Arity 2;
 column :: (Num a, Typeable a, Bits a) => [a] -> [a];
 column input = list_rotate (negate $ unArity salsa20_arity) $ take_same_length input $ fourfunc salsa20_arity r_as_list (map Rotation [7,9,13,18]) input;
 
-order1 :: [Integer];
-order1 = [12,0,4,8];
-
 shift_columns :: [[a]] -> [[a]];
--- shift_columns [] = error "empty shift_columns";
--- shift_columns (x:rest) = list_rotate (pred $ genericLength x) x : zipWith list_rotate (enumFrom 0) rest;
 shift_columns = zipWith list_rotate (enumFrom $ negate 1);
 
 unshift_columns :: [[a]] -> [[a]];
 unshift_columns = zipWith list_rotate $ enumFromThen 1 0;
-
-half :: (Num a, Typeable a, Bits a) => [[a]] -> [[a]];
-half = unshift_columns . map column . shift_columns . transpose;
 
 r_as_list :: forall a . (Typeable a, Bits a, Num a) => Rotation -> [a] -> a;
 -- this is how to trace a polymorphic function
@@ -106,16 +77,8 @@ take_same_length [] _ = [];
 take_same_length (_:r1) (h:r2) = h:take_same_length r1 r2;
 take_same_length _ _ = error "take_same_length: second list too short";
 
-others :: [a] -> [(a,[a])];
-others [] = error "empty";
-others [x] = [(x,[])];
-others (h:rest) = (h,rest):do {
-(p,q) <- others rest;
-return (p, h:q);
-};
-
 to_matrix :: Integer -> [a] -> [[a]];
-to_matrix n = unfoldr (\l -> case l of {[] -> Nothing; _ -> let {r = genericSplitAt n l} in assert (n == (genericLength $ fst r)) $ Just r});
+to_matrix n = unfoldr (\case {[] -> Nothing; l -> let {r = genericSplitAt n l} in assert (n == (genericLength $ fst r)) $ Just r});
 
 salsa20_diagonal :: [W];
 salsa20_diagonal = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574];
@@ -134,6 +97,7 @@ start_string = let {
 d i = [salsa20_diagonal !! i]
 } in d 0 ++ take 4 example_key ++ d 1 ++ [0x01040103,0x06020905,7,0] ++ d 2 ++ drop 4 example_key ++ d 3;
 
+-- we transpose first because we prefer to word with rows rather than columns
 one_round :: (Typeable a, Num a, Bits a) => [[a]] -> [[a]];
 one_round = unshift_columns . map column . shift_columns .transpose;
 
