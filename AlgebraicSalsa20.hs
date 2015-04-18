ccb20dbf5402618fa1fb71d1391685261d45e41d
Ken T Takusagawa <kenta@mit.edu>
2015-04-18 13:27:40 -0400

    Factor out algebraic stuff.

--- AlgebraicSalsa20.hs
+++ AlgebraicSalsa20.hs
@@ -1,43 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 -- enough rope to evaluate Salsa20 algebraically
 module Main(main) where {
-import Data.Bits(Bits(..));
 import Data.List(transpose);
 import Salsa20;
-data Algebraic a = Atom a | Xor (Algebraic a) (Algebraic a) | Rotate (Algebraic a) Int | Add (Algebraic a) (Algebraic a) deriving (Show,Eq);
-
-instance Num (Algebraic a) where {
-(+) x y = Add x y;
-(*) = error "Algebraic *";
-abs = error "Algebraic abs";
-signum = error "Algebraic signum";
-negate = error "Algebraic negate";
-fromInteger = error "Algebraic fromInteger";
-};
-
-instance (Eq a) => Bits (Algebraic a) where {
-xor x y = Xor x y;
-rotate x k = Rotate x k;
-(.&.) = error "Algebraic &";
-(.|.) = error "Algebraic |";
-complement = error "Algebraic complement";
-shift = error "Algebraic shift";
-bitSize = error "Algebraic bitSize";
-bitSizeMaybe = error "Algebraic bitSizeMaybe";
-isSigned = error "Algebraic isSigned";
-testBit = error "Algebraic testBit";
-bit = error "Algebraic bit";
-popCount = error "Algebraic popCount";
-};
-
-simplify :: (Eq a) => (a,Algebraic a) -> (Algebraic a) -> (Algebraic a);
-simplify v@(n,small) large = if small == large then Atom n
-else case large of {
-Atom _ -> large;
-Xor x y -> Xor (simplify v x) (simplify v y);
-Rotate x i -> Rotate (simplify v x) i;
-Add x y -> Add (simplify v x) (simplify v y);
-};
+import Algebraic;
 
 simplify_in_order :: [Assign] -> [Assign];
 simplify_in_order [] = [];
