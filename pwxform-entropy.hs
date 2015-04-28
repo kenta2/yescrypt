1fd7bbdaeeff6c3e7d853665affb5ad09058ad2b
Ken T Takusagawa <kenta@mit.edu>
2015-04-28 04:56:07 -0400

    use IntSet to save some memory, give answers and pointer to OEIS.

--- pwxform-entropy.hs
+++ pwxform-entropy.hs
@@ -1,17 +1,25 @@
 {-# LANGUAGE LambdaCase #-}
+-- computes values of OEIS A027417
 module Main where {
 import Data.List;
 --import Data.Set(Set);
-import qualified Data.Set as Set;
+--import qualified Data.Set as Set;
+-- Data.Set runs out of memory at 14
+import qualified Data.IntSet as Set;
+-- IntSet runs out at 16
+-- 15 takes 24 minutes.
 import System.Environment(getArgs);
+import Control.Exception(assert);
 to_pow_two :: Integer -> [Integer];
 to_pow_two n = genericTake ((2::Integer)^n) $ enumFrom 0;
 
-all_multiples :: Integer -> [Integer];
+all_multiples :: Integer -> [Int];
 all_multiples n = do {
 x <- to_pow_two n;
-y <- to_pow_two n;
-return $ x*y;
+y <- enumFromTo 0 x;
+let {z = x*y};
+assert (z < fromIntegral (maxBound :: Set.Key)) $ return ();
+return $ fromIntegral z;
 };
 
 main :: IO ();
@@ -19,5 +27,8 @@ main = getArgs >>= \case {
 [n] -> print $ Set.size $ Set.fromList $ all_multiples $ read n;
 _ -> error "n";
 };
+
+{- 2 7 26 90 340 1238 4647 17578 67592 259768 1004348 3902357 15202050
+59410557 232483840 -}
 
 } --end
