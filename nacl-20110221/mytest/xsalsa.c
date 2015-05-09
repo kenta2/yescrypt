d2e91304b894d0c49c0a25c2797f08143f65ec62
Ken T Takusagawa <kenta@mit.edu>
2015-05-09 02:45:23 -0400

    C runs

--- nacl-20110221/mytest/xsalsa.c
+++ nacl-20110221/mytest/xsalsa.c
@@ -1,22 +1,22 @@
-/*
-version 20080914
-D. J. Bernstein
-Public domain.
-*/
-
 #include "crypto_core_hsalsa20.h"
 #include "crypto_stream_salsa20.h"
 #include "crypto_stream.h"
-
+#include <stdio.h>
 static const unsigned char sigma[16] = "expand 32-byte k";
 
-int crypto_stream(
-        unsigned char *c,unsigned long long clen,
-  const unsigned char *n,
-  const unsigned char *k
-)
-{
+int main(void){
+
+  const unsigned char n[192/8]={0};
+  const unsigned char k[256/8]={0};
+  int i,row;
   unsigned char subkey[32];
   crypto_core_hsalsa20(subkey,n,k,sigma);
-  return crypto_stream_salsa20(c,clen,n + 16,subkey);
+  for(row=0;row<32;row+=4){
+    for(i=row;i<row+4;++i){
+      printf("%02x ",subkey[i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
 }
